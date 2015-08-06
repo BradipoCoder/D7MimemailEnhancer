b05fbe7082a7c2d6e41c36b325b3aefa539cfadf
Adam Jakab <jakabadambalazs@gmail.com>
2015-08-06 14:16:11 +0200

    debug.php fix

--- debug/debug.php
+++ debug/debug.php
@@ -1,9 +1,9 @@
 <?php
-/** @var array $message */
-
+/** @var array $TV - Template Variables */
+$TV = get_defined_vars();
+/** @var string $modulePath */
 $modulePath = drupal_get_path('module', 'mimemail_enhancer');
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,17 +19,18 @@ $modulePath = drupal_get_path('module', 'mimemail_enhancer');
 
     <hr />
     <h3>Your Mail Html("themed_body")</h3>
-    <pre><?php print htmlentities($message['themed_body']); ?></pre>
+    <pre><?php print htmlentities($TV['message']['themed_body']); ?></pre>
 
     <hr />
     <h3>Message</h3>
-    <pre><?php print htmlentities(print_r($message, true)); ?></pre>
+    <pre><?php print htmlentities(print_r($TV['message'], true)); ?></pre>
 
     <script type="text/javascript">
         var mme_debug = {
-            'themed_body': <?php print json_encode($message["themed_body"]); ?>
+            'themed_body': <?php print json_encode($TV['message']["themed_body"]); ?>
         };
     </script>
     <script type="text/javascript" src="/<?php print $modulePath; ?>/debug/debug.js"></script>
 </body>
 </html>
+
